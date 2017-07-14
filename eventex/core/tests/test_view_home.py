c877416374f650f8f8d2cba8c76edd65e6f4a166
thiagocoroa <macabroaff@hotmail.com>
2017-07-13 21:45:56 -0300

    adiciona link de palestrantes

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -7,12 +7,23 @@ from django.shortcuts import resolve_url as r
 class HomeTest(TestCase):
     """docstring for HomeTest"""  
     def setUp(self):
-    	self.response=self.client.get(r('home'))
+        self.response=self.client.get(r('home'))
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
     def test_subscription_link(self):
-    	expected= 'href="{}"'.format(r('subscriptions:new'))
-    	self.assertContains(self.response, expected)
+        expected= 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        contents=['Grace Hopper','http://hbn.link/hopper-pic',
+                    'Alan Turing','http://hbn.link/turing-pic']
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected='href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
